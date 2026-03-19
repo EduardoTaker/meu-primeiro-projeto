e3e4bb704d0cfe15d486a67cd0591667e468e15c
Dutra7n <eduardotsilva02@gmail.com>
2026-03-19 11:28:55 -0300

    Minha primeira versão do código

---
+++ ola.py
@@ -0,0 +1,7 @@
+# Meu primeiro código para o Git
+nome = input("Qual é o seu nome? ")
+print(f"Olá {nome}, agora você vai aprender Git de verdade!")
+
+print("---")
+print("1. Git: Guarda as versões no meu PC")
+print("2. GitHub: Guarda as versões na Nuvem")
